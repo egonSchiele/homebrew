38fd37267102af136c9c23a5231e242dfd793642
Aditya Bhargava <batman@scribd.com>
2012-03-10 12:25:47 -0800

    adding formula for soconnect

---
+++ Library/Formula/soconnect.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Soconnect < GithubGistFormula
+  url 'https://raw.github.com/gist/2012959/c7dc4faaec4c7e5e483a0467dc12150ce725f7e8/soconnect.d'
+  homepage 'https://gist.github.com/2012959'
+  md5 'bca8ce4640b1842f8cbd987b6dc27d77'
+end
+
