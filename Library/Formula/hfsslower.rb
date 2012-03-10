d5431b5039a6196d1ce8ab8f67ced1e11b64cb95
Aditya Bhargava <batman@scribd.com>
2012-03-10 11:25:38 -0800

    make that a github gist formula

--- Library/Formula/hfsslower.rb
+++ Library/Formula/hfsslower.rb
@@ -1,17 +1,7 @@
 require 'formula'
 
-class Hfsslower < Formula
-  url 'http://static.adit.io/brew/hfsslower.d'
-  homepage 'http://dtrace.org/blogs/brendan/2011/10/10/top-10-dtrace-scripts-for-mac-os-x/'
+class Hfsslower < GithubGistFormula
+  url 'https://raw.github.com/gist/2012605/fdc4181a14ca8a8b6de3292c420d135ab49235d5/hfsslower.d'
+  homepage 'https://gist.github.com/2012605'
   md5 '667ca213d8a202c209505fb183a1bf64'
-  version '0.0.1'
-
-  def install
-    bin.install("hfsslower.d")
-  end
-
-  def test
-    # requires sudo to test :(
-    system "true"
-  end
 end
