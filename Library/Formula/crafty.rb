81af1b1d3514d36d7ac84b00912b412c218fdcab
Aditya Bhargava <batman@scribd.com>
2012-05-12 19:32:48 -0500

    adding crafty and underscore

---
+++ Library/Formula/crafty.rb
@@ -0,0 +1,7 @@
+require 'formula'
+class Crafty < DownloadFormula
+  homepage 'http://craftyjs.com/'
+  url 'http://craftyjs.com/release/0.4.8/crafty-min.js'
+  md5 '6635caf2d27ddbeb70a690e0e90140fe'
+  version '0.4.8'
+end
---
+++ Library/Formula/underscore.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Underscore < DownloadFormula
+  homepage 'http://documentcloud.github.com/underscore/'
+  url 'http://documentcloud.github.com/underscore/underscore-min.js'
+  md5 '0899f764c85bbcc13206e3dd8318e678'
+  version '1.3.3'
+end
